6241d37a525cfad64be777319262272d8d7cb51f
Martinas03 <martinas1990@mail.ru>
2022-02-18 00:19:01 +0300

    added js and css files

---
+++ index.js
@@ -0,0 +1,17 @@
+var firstNameId = 'first-name'
+var firstNameEl = document.getElementById(firstNameId)
+var lastNameId = 'last-name'
+var lastNameEl = document.getElementById(lastNameId)
+var addressId = 'address'
+var addressEl = document.getElementById(addressId)
+var citiesId = 'cities'
+var citiesEl = document.getElementById(citiesId)
+var hobbiesId = 'hobbies'
+var hobbiesEl = document.getElementById(hobbiesId)
+var avatarwrapperId = 'avatar-wrapper'
+var avatarwrapperEl = document.getElementById(avatarwrapperId)
+var avatarId = 'avatar'
+var avatarEl = document.getElementById(avatarId)
+
+lastNameEl.className = 'error-input last-name-input default-input';
+lastNameEl.title = 'wrong last name';
